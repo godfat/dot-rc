f8729546117c121cc9a6fbf3d4b5a352996d7d81
Lin Jen-Shin <godfat@godfat.org>
2009-10-27 17:05:16 +0800

    [config.fish] single quotes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,17 @@
 
-function ls -d "color and utf-8 support"
+function ls -d 'color and utf-8 support'
   ls -Gw $argv
 end
 
-function ll -d "short for ls -lh"
+function ll -d 'short for ls -lh'
   ls -lh $argv
 end
 
-function la -d "short for ll -a"
+function la -d 'short for ll -a'
   ll -a $argv
 end
 
-function fish_prompt -d "custom prompt"
+function fish_prompt -d 'custom prompt'
   set git (show_git_branch)
   if test $git
     echo (set_color green)(prompt_pwd)(set_color cyan) $git(set_color normal)'> '
@@ -20,7 +20,7 @@ function fish_prompt -d "custom prompt"
   end
 end
 
-function show_git_dirty -d "show * if working directory is not clean"
+function show_git_dirty -d 'show * if working directory is not clean'
   if test (git status 2> /dev/null | tail -n1) != 'nothing to commit (working directory clean)' 2> /dev/null
     echo '*'
   else
@@ -28,6 +28,6 @@ function show_git_dirty -d "show * if working directory is not clean"
   end
 end
 
-function show_git_branch -d "show git branch if any"
+function show_git_branch -d 'show git branch if any'
   git symbolic-ref HEAD 2> /dev/null | sed 's/refs\/heads\/\(.*\)/'(show_git_dirty)'\1/'
 end
