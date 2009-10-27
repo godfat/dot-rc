a0744935bdb5f34d1033e0af2cd7580ea685153b
Lin Jen-Shin <godfat@godfat.org>
2009-10-27 16:50:05 +0800

    [config.fish] fixed spacing errors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,7 @@ end
 function fish_prompt -d "custom prompt"
   set git (show_git_branch)
   if test $git
-    printf '%s%s%s%s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color cyan) $git (set_color normal)
+    printf '%s%s%s %s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color cyan) $git (set_color normal)
   else
     printf '%s %s%s%s> ' (whoami) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
   end
@@ -22,10 +22,12 @@ end
 
 function show_git_dirty -d "show * if working directory is not clean"
   if test (git status 2> /dev/null | tail -n1) != 'nothing to commit (working directory clean)' 2> /dev/null
-    echo ' *'
+    echo '*'
+  else
+    echo
   end
 end
 
 function show_git_branch -d "show git branch if any"
-  git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\/\(.*\)/'(show_git_dirty)'\1/'
+  git symbolic-ref HEAD 2> /dev/null | sed 's/refs\/heads\/\(.*\)/'(show_git_dirty)'\1/'
 end
