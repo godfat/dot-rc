14bc07f4e825a6f7fc4efaa05d7e1d94479d5e1f
Lin Jen-Shin <godfat@godfat.org>
2020-12-29 21:01:04 +0800

    Need this to enable userChrome and userContent

--- .config/firefox/prefs.js
+++ .config/firefox/prefs.js
@@ -24,4 +24,5 @@ user_pref("tongwentang.contextMenu.InputTra", false);
 user_pref("tongwentang.contextMenu.PageSim", false);
 user_pref("tongwentang.contextMenu.TextZoom", false);
 user_pref("tongwentang.contextMenuIcon", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("ui.context_menus.after_mouseup", true);
