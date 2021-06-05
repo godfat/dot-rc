93516ad731b772f9d6bc5df4cc888622a7bdb973
Lin Jen-Shin <godfat@godfat.org>
2021-06-06 01:20:25 +0800

    Without this `userChrome.css` won't work properly
    under Firefox 89+. See:
    
    * https://github.com/stonecrusher/simpleMenuWizard/pull/47
    *
    https://www.reddit.com/r/firefox/comments/nsvg5r/rightclick_context_menu_shortcuts_gone_in_ff89/h0omfjn/

--- .config/firefox/prefs.js
+++ .config/firefox/prefs.js
@@ -19,3 +19,4 @@ user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("ui.context_menus.after_mouseup", true);
+user_pref("widget.macos.native-context-menus", false);
